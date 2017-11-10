4a0bb3f4aa11f8e967585a721a019dd66b52f536
min min <min min@DESKTOP-KHAN9PU>
2017-11-10 14:58:15 +0700

    updatefrm Custommer

---
+++ VideoRental/frmCustomer.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace VideoRental
+{
+    partial class frmCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.btnback = new System.Windows.Forms.Button();
+            this.dtGvCustomer = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGvCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CustomerID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CustomerName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Address";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "PhoneNumber";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(279, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(128, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "QUẢN LÝ KHÁCH HÀNG";
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Location = new System.Drawing.Point(181, 44);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(148, 20);
+            this.txtCustomerID.TabIndex = 5;
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(181, 78);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(148, 20);
+            this.txtCustomerName.TabIndex = 6;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(181, 114);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(148, 20);
+            this.txtAddress.TabIndex = 7;
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(181, 143);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(148, 20);
+            this.txtPhoneNumber.TabIndex = 8;
+            // 
+            // btnthem
+            // 
+            this.btnthem.Location = new System.Drawing.Point(444, 47);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(75, 23);
+            this.btnthem.TabIndex = 9;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Location = new System.Drawing.Point(444, 81);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(75, 23);
+            this.btnxoa.TabIndex = 10;
+            this.btnxoa.Text = "Sửa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnsua_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.Location = new System.Drawing.Point(444, 112);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(75, 23);
+            this.btnsua.TabIndex = 11;
+            this.btnsua.Text = "Xóa";
+            this.btnsua.UseVisualStyleBackColor = true;
+            this.btnsua.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // btnback
+            // 
+            this.btnback.Location = new System.Drawing.Point(444, 146);
+            this.btnback.Name = "btnback";
+            this.btnback.Size = new System.Drawing.Size(75, 23);
+            this.btnback.TabIndex = 12;
+            this.btnback.Text = "Trở về";
+            this.btnback.UseVisualStyleBackColor = true;
+            this.btnback.Click += new System.EventHandler(this.btnback_Click);
+            // 
+            // dtGvCustomer
+            // 
+            this.dtGvCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGvCustomer.Location = new System.Drawing.Point(52, 184);
+            this.dtGvCustomer.Name = "dtGvCustomer";
+            this.dtGvCustomer.Size = new System.Drawing.Size(571, 150);
+            this.dtGvCustomer.TabIndex = 13;
+            this.dtGvCustomer.SelectionChanged += new System.EventHandler(this.dtGvCustomer_SelectionChanged);
+            // 
+            // frmCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 346);
+            this.Controls.Add(this.dtGvCustomer);
+            this.Controls.Add(this.btnback);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btnthem);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.txtCustomerID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCustomer";
+            this.Text = "frmCustomer";
+            this.Load += new System.EventHandler(this.frmCustomer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtGvCustomer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Button btnsua;
+        private System.Windows.Forms.Button btnback;
+        private System.Windows.Forms.DataGridView dtGvCustomer;
+    }
+}
---
+++ VideoRental/frmCustomer.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entities;
+using BLL;
+
+namespace VideoRental
+{
+    public partial class frmCustomer : Form
+    {
+        List<eCustomer> listkh;
+        CustomerBLL khbll;
+        public frmCustomer()
+        {
+            InitializeComponent();
+            listkh = new List<eCustomer>();
+            khbll = new CustomerBLL();
+            listkh = khbll.GetAllCustomer();
+
+
+            // LoadDataGridView(dtGvCustomer, listkh);
+
+        }
+        public void LoadDataGridView(DataGridView dgv, List<eCustomer> l)
+        {
+
+        }
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmCustomer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtGvCustomer_SelectionChanged(object sender, EventArgs e)
+        {
+            txtCustomerID.Text = dtGvCustomer.CurrentRow.Cells[0].Value.ToString();
+            txtCustomerName.Text = dtGvCustomer.CurrentRow.Cells[1].Value.ToString();
+            txtAddress.Text = dtGvCustomer.CurrentRow.Cells[2].Value.ToString();
+            txtPhoneNumber.Text = dtGvCustomer.CurrentRow.Cells[3].Value.ToString();
+
+        }
+
+        private void btnback_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Do you want to exit?", "Noti", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (DialogResult.OK == r)
+                this.Close();
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            eCustomer cus = new eCustomer();
+            cus.CustomerID = Convert.ToInt32(txtCustomerID.Text);
+            cus.CustomerName = txtCustomerName.Text;
+            cus.Address = txtAddress.Text;
+            cus.PhoneNumber = txtPhoneNumber.Text;
+
+
+            khbll.AddCustomer(cus);
+            LoadDataGridView(dtGvCustomer, khbll.GetAllCustomer());
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            int customerid = Convert.ToInt32(txtCustomerID.Text);
+            if (khbll.Delete(customerid))
+            {
+                MessageBox.Show("Thành Công");
+                listkh = khbll.GetAllCustomer();
+                LoadDataGridView(dtGvCustomer, listkh);
+
+            }
+            else
+            {
+                MessageBox.Show("Thất bại");
+            }
+
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            eCustomer cus = new eCustomer();
+            cus.CustomerID = Convert.ToInt32(txtCustomerID.Text);
+            cus.CustomerName = txtCustomerName.Text;
+            cus.Address = txtAddress.Text;
+            cus.PhoneNumber = txtPhoneNumber.Text;
+
+            khbll.Update(cus);
+
+            LoadDataGridView(dtGvCustomer, khbll.GetAllCustomer());
+           
+
+        }
+    }
+}
