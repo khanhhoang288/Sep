ecf2535a92a99bd865751a89c38542f2e63ccce5
khanhhoang288 <khanhhoang288@gmail.com>
2017-11-12 00:03:45 +0700

    resertavion form , button add reservation

---
+++ VideoRental/frmReservation.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace VideoRental
+{
+    partial class frmReservation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvReservation = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.dtpReservation = new System.Windows.Forms.DateTimePicker();
+            this.lblReservationID = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReservation)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvReservation
+            // 
+            this.dgvReservation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReservation.Location = new System.Drawing.Point(78, 130);
+            this.dgvReservation.Name = "dgvReservation";
+            this.dgvReservation.Size = new System.Drawing.Size(505, 150);
+            this.dgvReservation.TabIndex = 0;
+            this.dgvReservation.RowStateChanged += new System.Windows.Forms.DataGridViewRowStateChangedEventHandler(this.dgvReservation_RowStateChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(49, 321);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(169, 321);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Location = new System.Drawing.Point(159, 59);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(96, 20);
+            this.txtCustomerID.TabIndex = 2;
+            // 
+            // dtpReservation
+            // 
+            this.dtpReservation.Location = new System.Drawing.Point(393, 59);
+            this.dtpReservation.Name = "dtpReservation";
+            this.dtpReservation.Size = new System.Drawing.Size(200, 20);
+            this.dtpReservation.TabIndex = 3;
+            // 
+            // lblReservationID
+            // 
+            this.lblReservationID.AutoSize = true;
+            this.lblReservationID.Location = new System.Drawing.Point(12, 9);
+            this.lblReservationID.Name = "lblReservationID";
+            this.lblReservationID.Size = new System.Drawing.Size(85, 13);
+            this.lblReservationID.TabIndex = 4;
+            this.lblReservationID.Text = "lblReservationID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "customer ID";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(294, 321);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // frmReservation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 361);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblReservationID);
+            this.Controls.Add(this.dtpReservation);
+            this.Controls.Add(this.txtCustomerID);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvReservation);
+            this.Name = "frmReservation";
+            this.Text = "frmReservation";
+            this.Load += new System.EventHandler(this.frmReservation_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReservation)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvReservation;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.DateTimePicker dtpReservation;
+        private System.Windows.Forms.Label lblReservationID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ VideoRental/frmReservation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BLL;
+using Entities;
+
+namespace VideoRental
+{
+    public partial class frmReservation : Form
+    {
+        List<eReservation> lsre;
+        ReservationBLL rebll;
+        public frmReservation()
+        {
+            InitializeComponent();
+
+            lsre = new List<eReservation>();
+            rebll = new ReservationBLL();
+
+            lsre = rebll.getAllReservation();
+
+            LoadDataGridView(dgvReservation, lsre);
+        }
+
+        private void frmReservation_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void LoadDataGridView(DataGridView dgv, List<eReservation> l)
+        {
+            dgv.DataSource =l;
+        }
+
+        private void dgvReservation_RowStateChanged(object sender, DataGridViewRowStateChangedEventArgs e)
+        {
+            if (dgvReservation.SelectedRows.Count > 0)
+            {
+                lblReservationID.Text = e.Row.Cells[1].Value.ToString();
+                txtCustomerID.Text= e.Row.Cells[0].Value.ToString();
+                dtpReservation.Text= e.Row.Cells[2].Value.ToString();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            eReservation er = new eReservation();
+
+            er.CustomerID = Convert.ToInt32(txtCustomerID.Text);
+            er.ReservationDate = Convert.ToDateTime(dtpReservation.Text);
+
+            rebll.insertReservation(er);
+
+
+
+            LoadDataGridView(dgvReservation, rebll.getAllReservation());
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
