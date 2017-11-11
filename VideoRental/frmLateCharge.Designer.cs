d9dd3faa5e4a371d4fa2c1a35ec7444468a33654
khanhhoang288 <khanhhoang288@gmail.com>
2017-11-11 10:09:34 +0700

    a

---
+++ VideoRental/frmLateCharge.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace VideoRental
+{
+    partial class frmLateCharge
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvLateCharge = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTotalLateCharge = new System.Windows.Forms.TextBox();
+            this.rdbAllLateCharge = new System.Windows.Forms.RadioButton();
+            this.rdbPartLateCharge = new System.Windows.Forms.RadioButton();
+            this.btnPaid = new System.Windows.Forms.Button();
+            this.txtPartLateCharge = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLateCharge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvLateCharge
+            // 
+            this.dgvLateCharge.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLateCharge.Location = new System.Drawing.Point(30, 28);
+            this.dgvLateCharge.Name = "dgvLateCharge";
+            this.dgvLateCharge.Size = new System.Drawing.Size(520, 182);
+            this.dgvLateCharge.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total Late Charge";
+            // 
+            // txtTotalLateCharge
+            // 
+            this.txtTotalLateCharge.Location = new System.Drawing.Point(219, 221);
+            this.txtTotalLateCharge.Name = "txtTotalLateCharge";
+            this.txtTotalLateCharge.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalLateCharge.TabIndex = 2;
+            // 
+            // rdbAllLateCharge
+            // 
+            this.rdbAllLateCharge.AutoSize = true;
+            this.rdbAllLateCharge.Location = new System.Drawing.Point(60, 267);
+            this.rdbAllLateCharge.Name = "rdbAllLateCharge";
+            this.rdbAllLateCharge.Size = new System.Drawing.Size(36, 17);
+            this.rdbAllLateCharge.TabIndex = 3;
+            this.rdbAllLateCharge.TabStop = true;
+            this.rdbAllLateCharge.Text = "All";
+            this.rdbAllLateCharge.UseVisualStyleBackColor = true;
+            // 
+            // rdbPartLateCharge
+            // 
+            this.rdbPartLateCharge.AutoSize = true;
+            this.rdbPartLateCharge.Location = new System.Drawing.Point(60, 290);
+            this.rdbPartLateCharge.Name = "rdbPartLateCharge";
+            this.rdbPartLateCharge.Size = new System.Drawing.Size(44, 17);
+            this.rdbPartLateCharge.TabIndex = 3;
+            this.rdbPartLateCharge.TabStop = true;
+            this.rdbPartLateCharge.Text = "Part";
+            this.rdbPartLateCharge.UseVisualStyleBackColor = true;
+            // 
+            // btnPaid
+            // 
+            this.btnPaid.Location = new System.Drawing.Point(366, 290);
+            this.btnPaid.Name = "btnPaid";
+            this.btnPaid.Size = new System.Drawing.Size(75, 23);
+            this.btnPaid.TabIndex = 4;
+            this.btnPaid.Text = "Paid";
+            this.btnPaid.UseVisualStyleBackColor = true;
+            // 
+            // txtPartLateCharge
+            // 
+            this.txtPartLateCharge.Location = new System.Drawing.Point(178, 287);
+            this.txtPartLateCharge.Name = "txtPartLateCharge";
+            this.txtPartLateCharge.Size = new System.Drawing.Size(100, 20);
+            this.txtPartLateCharge.TabIndex = 2;
+            // 
+            // frmLateCharge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 371);
+            this.Controls.Add(this.btnPaid);
+            this.Controls.Add(this.rdbPartLateCharge);
+            this.Controls.Add(this.rdbAllLateCharge);
+            this.Controls.Add(this.txtPartLateCharge);
+            this.Controls.Add(this.txtTotalLateCharge);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvLateCharge);
+            this.Name = "frmLateCharge";
+            this.Text = "frmLateCharge";
+            this.Load += new System.EventHandler(this.frmLateCharge_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLateCharge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvLateCharge;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTotalLateCharge;
+        private System.Windows.Forms.RadioButton rdbAllLateCharge;
+        private System.Windows.Forms.RadioButton rdbPartLateCharge;
+        private System.Windows.Forms.Button btnPaid;
+        private System.Windows.Forms.TextBox txtPartLateCharge;
+    }
+}
---
+++ VideoRental/frmLateCharge.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BLL;
+using Entities;
+
+namespace VideoRental
+{
+    public partial class frmLateCharge : Form
+    {
+        List<eLateCharge> listlc;
+        LateChargeBLL lcbll;
+
+        public frmLateCharge()
+        {
+            InitializeComponent();
+
+            listlc = new List<eLateCharge>();
+            lcbll = new LateChargeBLL();
+
+            //listlc = lcbll.getAllLateCharge();
+            listlc = lcbll.getLateChargeByCustomerID(2);
+            LoadDataGridView(dgvLateCharge, listlc);
+        }
+
+        private void frmLateCharge_Load(object sender, EventArgs e)
+        {
+            txtTotalLateCharge.Text = lcbll.sumLateChargeByCustomerID(2).ToString();
+        }
+
+        public void LoadDataGridView(DataGridView dgv, List<eLateCharge> l)
+        {
+            dgv.DataSource = l;
+        }
+    }
+}
